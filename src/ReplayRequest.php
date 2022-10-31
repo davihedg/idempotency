8601e37d2c32bfb7dc63de552e6d4a2243cb1a4a
d.batrakova <d.batrakova@iilab.ru>
2022-10-31 16:48:51 +0300

    fix: hash signature log info

--- src/ReplayRequest.php
+++ src/ReplayRequest.php
@@ -3,6 +3,7 @@
 namespace Bvtterfly\Replay;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ReplayRequest
 {
@@ -17,6 +18,24 @@ class ReplayRequest
 
         $bodyParam = $request->all();
         ksort($bodyParam);
+
+        $signature = json_encode(
+            [
+                $request->ip(),
+                $request->path(),
+                $bodyParam
+            ]
+        );
+        $hash = hash($hashAlgo, json_encode(
+            [
+                $request->ip(),
+                $request->path(),
+                $bodyParam
+            ]
+        ));
+
+        Log::info("request signature: $signature");
+        Log::info("hash signature: $hash");
 
         return hash($hashAlgo, json_encode(
             [
